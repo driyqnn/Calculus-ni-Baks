88168ea72d48cabd31b2dd6292f115ad9ca5dbba
driyqnn <adridump121@gmail.com>
2026-01-14 11:29:55 +0800

    Remove analytics initialization from src/main.tsx
    Removed analytics initialization from main entry file.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // Add this to src/main.tsx
-import { initAnalytics } from './draeAnalytics';
-
-// Initialize after the DOM loads
-initAnalytics('https://discord.com/api/webhooks/1400412328259752016/NpBiWlT-3R3GT6ZprLhv4Iqtu1-JPSUmtv2mIBbqHbfyh3BZM8n1Ayg45juxzDudLNmU');
-
 
 createRoot(document.getElementById("root")!).render(<App />);
